d7042139493710787184e8e60944c02b8ae09c06
Michael Phelps <michaeljphelps1978@gmail.com>
2020-12-04 00:28:24 -0600

    :sparkles: Day 1 and 2
    Implemented CPU, Ram Read/Wriite, HLT, LDI, PRN, Load,  and MULT operations

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,42 +1,120 @@
 """CPU functionality."""
 
 import sys
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+MUL = 0b10100010
+
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.running = False
+        self.ram = [0] * 256
+        self.registers = [0] * 8
+        self.pc = 0                  # Program Counter
+        self.ops = {}
+        self.ops[LDI] = self.LDI
+        self.ops[PRN] = self.PRN
+        self.ops[HLT] = self.HLT
+        self.ops[MUL] = self.MUL
+
+
+    def ram_read(self, address):
+        print(self.registers[address])
+
+    def ram_write(self, address, value):
+        self.registers[address] = value
+
+    def HLT(self):
+        self.running = False
+
+    def LDI(self):
+        address = self.ram[self.pc + 1]
+        value = self.ram[self.pc + 2]
+
+        self.ram_write(address, value)
+        self.pc += 3
+
+    def PRN(self):
+        address = self.ram[self.pc + 1]
+        self.ram_read(address)
+        self.pc += 2
+
+    def MUL(self):
+        reg_a = self.ram[self.pc + 1]
+        reg_b = self.ram[self.pc + 2]
+
+        self.alu('MUL', reg_a, reg_b)
+        self.pc += 3
+
+
 
     def load(self):
         """Load a program into memory."""
+        try:
+            if len(sys.argv) < 2:
+                print(f'Error from {sys.argv[0]}: Missing Filename Argument')
+                print(f'Usage: {sys.argv[0]} <filename>')
 
-        address = 0
+                sys.exit(1)
 
-        # For now, we've just hardcoded a program:
+            # counter to add to ram
+            ram_index = 0
+            path = sys.argv[1]
+            with open('./examples/' + path) as f:
+                lines = f.readlines()
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+            program = []
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+            for line in lines:
+                line = line.strip()
+                if '#' in line:
+                    comment_start = line.index('#')
+                    if comment_start == 0:
+                        continue
+                    else:
+                        binary = int(line[:comment_start].strip(), 2)
+                        program.append(binary)
 
+                elif line:
+                    binary = int(line, 2)
+                    program.append(binary)
+
+            address = 0
+            for instruction in program:
+                self.ram[address] = instruction
+                address += 1
+
+        except FileNotFoundError:
+            print(f'Error from {sys.argv[0]}: File Not Found')
+            print('Please check the file name and try again')
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+            self.registers[reg_a] += self.registers[reg_b]
+
+        elif op == "SUB":
+            self.registers[reg_a] -= self.registers[reg_b]
+
+        elif op == "MUL":
+            self.registers[reg_a] *= self.registers[reg_b]
+
+        elif op == "AND":
+            self.registers[reg_a] &= self.registers[reg_b]
+
+        elif op == "OR":
+            self.registers[reg_a] |= self.registers[reg_b]
+
+        elif op == "XOR":
+            self.registers[reg_a] ^= self.registers[reg_b]
+
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +126,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +140,8 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        self.running = True
+
+        while self.running:
+            ir = self.ram[self.pc]
+            self.ops[ir]()
